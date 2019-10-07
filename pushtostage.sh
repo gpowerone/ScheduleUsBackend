1594b23f14c53c68fe1037136ea40083d05291f9
Michael Mcdermott <michaelmcdermott@Michaels-iPro.fios-router.home>
2019-10-06 21:32:17 -0400

    Progress update

---
+++ pushtostage.sh
@@ -0,0 +1,3 @@
+scp -i "SchedusStage.pem" server.js ec2-user@ec2-3-81-149-114.compute-1.amazonaws.com:/home/ec2-user/schdus
+scp -i "SchedusStage.pem" -r libraries ec2-user@ec2-3-81-149-114.compute-1.amazonaws.com:/home/ec2-user/schdus
+scp -i "SchedusStage.pem" package.json ec2-user@ec2-3-81-149-114.compute-1.amazonaws.com:/home/ec2-user/schdus
